50dbe4ddcdc566c0330ad4d0b195e7ed096ae34a
JotaPedroo <jpcfernandes93@gmail.com>
2024-05-23 15:45:38 +0100

    Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+//im targeting both variables and when we click, it is going to add or remove the hamburger icon
